4fc67dba14189b074b27c610afb43dc22ba18014
associatesreet <associatesreet@gmail.com>
2025-09-23 03:30:55 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,8 +49,7 @@ export default function Navbar({
     >
       <div className="container flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          {/* Replace text logo with GIF */}
-          <img src="/logo.gif" alt="Reet Associates" className="h-10 w-auto" />
+        <img src="/logo.gif" alt="Reet Associates" className="h-10 w-auto" />
         </div>
 
         {/* Desktop Navigation */}
